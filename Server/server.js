bbddd97db71512d36242d5099b5cee15c2f54eca
Veda H Mata <hmata.veda@gmail.com>
2022-06-23 19:10:47 -0400

    login-registration

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const userRoutes = require("./Routes/user.routes");
+
 const app = express();
 const PORT = process.env.PORT;
 
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+userRoutes(app);
 
 app.listen(8000, () => {
   console.log(`Server is running  and Listening at the ${PORT} !!!!!`);
