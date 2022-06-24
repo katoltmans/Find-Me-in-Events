f692b7aef4d9f5a69b57c3d11da440de36485766
Veda H Mata <hmata.veda@gmail.com>
2022-06-24 11:57:56 -0400

    AuthOVclient side

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,30 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import "./App.css";
-// Import components
 import Navbar from "./components/Navbar";
 import LoginRegistrationForm from "./components/LoginRegistrationForm";
+import HomePage from "./components/homePage";
 
 function App() {
-    // declare state
-    const [events, setEvents] = useState([]);
-    const [user, setUser] = useState({});
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return (
-        <BrowserRouter>
-            <div>
-                <Navbar user={user} setUser={setUser} isLoggedIn={isLoggedIn} />
-                Hello World!
-                <Routes>
-                    <Route
-                        path="/register/login"
-                        element={<LoginRegistrationForm />}
-                    />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        <Routes>
+          <Route
+            path="/"
+            element={<LoginRegistrationForm setIsLoggedIn={setIsLoggedIn} />}
+          />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- Client/src/components/LoginRegistrationForm.js
+++ Client/src/components/LoginRegistrationForm.js
@@ -2,236 +2,208 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {
-    Box,
-    Grid,
-    Paper,
-    TextField,
-    Typography,
-    Button,
-    ListItem,
-    List,
+  Box,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  Button,
+  ListItem,
+  List,
 } from "@mui/material";
 
-const LoginRegistrationForm = () => {
-    const navigate = useNavigate();
-    const [user, setUser] = useState({
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: "",
-        confirm_password: "",
+const LoginRegistrationForm = ({ setIsLoggedIn }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    emailId: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [regErr, setRegisterErrors] = useState([]);
+  const [logErr, setLogErrors] = useState("");
+
+  const onChangeHandler = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
     });
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState([]);
+  };
 
-    const onChangeHandler = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value,
-        });
-    };
+  const OnSubmitHandlerRegistration = (e) => {
+    console.log("submitting registration");
+    e.preventDefault();
+    console.log("USER: ", user);
+    axios
+      .post("http://localhost:8000/api/register", user, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log("successfully registered", res.data);
+        setIsLoggedIn(true);
+        navigate("/home");
+      })
+      .catch((err) => {
+        setLogErrors("");
+        const errorarray = (obj) => {
+          const arr = [];
+          for (let keys of Object.keys(obj)) {
+            arr.push(obj[keys].message);
+          }
+          setRegisterErrors(arr);
+        };
+        errorarray(err.response.data.errors);
+      });
+  };
 
-    const OnSubmitHandlerRegistration = (e) => {
-        console.log("submitting registration");
-        e.preventDefault();
+  const OnSubmitHandlerLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:8000/api/login",
+        {
+          emailId: user.emailId,
+          password: user.password,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        console.log("successfully loggedIn", res.data);
+        setIsLoggedIn(true);
+        navigate("/home");
+      })
+      .catch((err) => {
+        setRegisterErrors([]);
+        setLogErrors(err.response.data.Error);
+        console.log("Error with login post request client", err);
+      });
+  };
 
-        console.log("USER: ", user);
-        // Post request to create a new author
-        axios
-            .post("http://localhost:8000/api/register", user)
-            .then((res) => {
-                console.log(res);
-                console.log(res.data);
-                if (!res.data?.errors) {
-                    //set user if successfully registered
-                    setUser(user);
-                    navigate("/");
-                } else {
-                    // Add errors to be d
-                    setErrors(res.data.errors);
-                }
-            })
-            .catch((err) => {
-                console.log(
-                    "Error with post registration request (client)",
-                    err
-                );
-                setErrors(err.response.data?.error?.errors);
-                console.log("ERROR:", errors);
-            });
-    };
-
-    const OnSubmitHandlerLogin = (e) => {
-        console.log("submitting login");
-        e.preventDefault();
-
-        console.log("USER: ", user);
-
-        // Post request to create a new author
-        axios
-            .post("http://localhost:8000/api/login")
-            // {
-            //     email: email,
-            //     password: password,
-            // }
-            .then((res) => {
-                console.log(res);
-                console.log(res.data);
-                if (!res.data.errors) {
-                    //setIsLoggedIn
-                    setUser(res.data.user);
-                    navigate("/hacks/view");
-                } else {
-                    setErrors(res.data.errors);
-                }
-            })
-            .catch((err) => {
-                console.log("Error with login post request (client)", err);
-            });
-    };
-
-    return (
-        <Paper elevation={2} sx={{ p: 5, m: 5, display: "flex" }}>
-            <Box sx={{ flexGrow: 1, mr: 1 }}>
-                <Typography variant="h3" component="h1" sx={{ mb: 3 }}>
-                    Registration
-                </Typography>
-                {errors ? (
-                    <List sx={{ mb: 5 }}>
-                        {errors.map((error, index) => {
-                            return (
-                                <ListItem
-                                    key={index}
-                                    sx={{ color: "error.main" }}
-                                >
-                                    {error}
-                                </ListItem>
-                            );
-                        })}
-                    </List>
-                ) : null}
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={3}>
-                        <Grid container item spacing={1}>
-                            <Grid item xs={4}>
-                                <TextField
-                                    fullWidth
-                                    name="first_name"
-                                    label="First Name"
-                                    variant="outlined"
-                                    onChange={onChangeHandler}
-                                />
-                            </Grid>
-                            <Grid item xs={4}>
-                                <TextField
-                                    fullWidth
-                                    name="last_name"
-                                    label="Last Name"
-                                    variant="outlined"
-                                    onChange={onChangeHandler}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Grid container item spacing={3}>
-                            <Grid item xs={8}>
-                                <TextField
-                                    fullWidth
-                                    name="email"
-                                    label="Email"
-                                    variant="outlined"
-                                    onChange={onChangeHandler}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Grid container item spacing={3}>
-                            <Grid item xs={4}>
-                                <TextField
-                                    fullWidth
-                                    name="password"
-                                    type="password"
-                                    label="Password"
-                                    variant="outlined"
-                                    onChange={onChangeHandler}
-                                />
-                            </Grid>
-                            <Grid item xs={4}>
-                                <TextField
-                                    fullWidth
-                                    name="confirm_password"
-                                    type="password"
-                                    label="Confirm Password"
-                                    variant="outlined"
-                                    onChange={onChangeHandler}
-                                />
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Button
-                        variant="contained"
-                        sx={{ mt: 3 }}
-                        onClick={OnSubmitHandlerRegistration}
-                    >
-                        Submit
-                    </Button>
-                </Box>
-            </Box>
-            <Box sx={{ flexGrow: 1, ml: 5 }}>
-                <Typography variant="h3" component="h1" sx={{ mb: 3 }}>
-                    Login
-                </Typography>
-                {errors ? (
-                    <Typography sx={{ color: "error.main", mb: 5 }}>
-                        {errors}
-                    </Typography>
-                ) : null}
-                {/* {errors ? (errors.map((errorMessage, index) => {
-                <>
-                <Typography sx={{ color: "error.main", mb: 5 }}>
-                    {errors}
-                </Typography>
-                <Divider/>
-                </>
-            }) : null} */}
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={3}>
-                        <Grid container item spacing={3}>
-                            <Grid item xs={8}>
-                                <TextField
-                                    fullWidth
-                                    name="email"
-                                    label="Email"
-                                    variant="outlined"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Grid container item spacing={3}>
-                            <Grid item xs={8}>
-                                <TextField
-                                    fullWidth
-                                    name="password"
-                                    type="password"
-                                    label="Password"
-                                    variant="outlined"
-                                    onChange={(e) =>
-                                        setPassword(e.target.value)
-                                    }
-                                />
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Button
-                        variant="contained"
-                        sx={{ mt: 3 }}
-                        onClick={OnSubmitHandlerLogin}
-                    >
-                        Login
-                    </Button>
-                </Box>
-            </Box>
-        </Paper>
-    );
+  return (
+    <Paper elevation={2} sx={{ p: 5, m: 5, display: "flex" }}>
+      <Box sx={{ flexGrow: 1, mr: 1 }}>
+        <Typography variant="h3" component="h1" sx={{ mb: 3 }}>
+          Registration
+        </Typography>
+        {regErr && (
+          <List sx={{ mb: 5 }}>
+            {regErr.map((error, index) => {
+              return (
+                <ListItem key={index} sx={{ color: "error.main" }}>
+                  {error}
+                </ListItem>
+              );
+            })}
+          </List>
+        )}
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={3}>
+            <Grid container item spacing={1}>
+              <Grid item xs={4}>
+                <TextField
+                  fullWidth
+                  name="firstName"
+                  label="First Name"
+                  variant="outlined"
+                  onChange={onChangeHandler}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <TextField
+                  fullWidth
+                  name="lastName"
+                  label="Last Name"
+                  variant="outlined"
+                  onChange={onChangeHandler}
+                />
+              </Grid>
+            </Grid>
+            <Grid container item spacing={3}>
+              <Grid item xs={8}>
+                <TextField
+                  fullWidth
+                  name="emailId"
+                  label="EmailId"
+                  variant="outlined"
+                  onChange={onChangeHandler}
+                />
+              </Grid>
+            </Grid>
+            <Grid container item spacing={3}>
+              <Grid item xs={4}>
+                <TextField
+                  fullWidth
+                  name="password"
+                  type="password"
+                  label="Password"
+                  variant="outlined"
+                  onChange={onChangeHandler}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <TextField
+                  fullWidth
+                  name="confirmPassword"
+                  type="password"
+                  label="Confirm Password"
+                  variant="outlined"
+                  onChange={onChangeHandler}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Button
+            variant="contained"
+            sx={{ mt: 3 }}
+            onClick={OnSubmitHandlerRegistration}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ flexGrow: 1, ml: 5 }}>
+        <Typography variant="h3" component="h1" sx={{ mb: 3 }}>
+          Login
+        </Typography>
+        <List sx={{ mb: 5 }}>{logErr && <p>{logErr}!!</p>}</List>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={3}>
+            <Grid container item spacing={3}>
+              <Grid item xs={8}>
+                <TextField
+                  fullWidth
+                  name="emailId"
+                  label="EmailId"
+                  variant="outlined"
+                  onChange={onChangeHandler}
+                />
+              </Grid>
+            </Grid>
+            <Grid container item spacing={3}>
+              <Grid item xs={8}>
+                <TextField
+                  fullWidth
+                  name="password"
+                  type="password"
+                  label="Password"
+                  variant="outlined"
+                  onChange={onChangeHandler}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Button
+            variant="contained"
+            sx={{ mt: 3 }}
+            onClick={OnSubmitHandlerLogin}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Paper>
+  );
 };
 
 export default LoginRegistrationForm;
