a783f64e2274fc3263cae48d0bd57a29f5ad6a7c
katoltmans <katoltmans@msn.com>
2022-06-25 19:10:22 -0400

    FIx authentication issue

--- Server/Routes/event.routes.js
+++ Server/Routes/event.routes.js
@@ -2,25 +2,29 @@ const EventController = require("../Controllers/event.controller");
 const authenticate = require("../Config/authentication.middleware");
 
 module.exports = (app) => {
-  //all events routes....
-  app.get("/api/events", authenticate, EventController.getAllEvents);
-  app.get("/api/events/:id", authenticate, EventController.getOneEvent);
-  app.post("/api/events", authenticate, EventController.createEvent);
-  app.put("/api/events/:id", authenticate, EventController.updateOneEvent);
-  app.delete("/api/events/:id", authenticate, EventController.deleteOneEvent);
+    //all events routes....
+    app.get("/api/events", authenticate, EventController.getAllEvents);
+    app.get("/api/events/:id", authenticate, EventController.getOneEvent);
+    app.post("/api/events", authenticate, EventController.createEvent);
+    app.put("/api/events/:id", authenticate, EventController.updateOneEvent);
+    app.delete("/api/events/:id", authenticate, EventController.deleteOneEvent);
 
-  //all comment routes...
-  app.put("/api/comment/:eventId", authenticate, EventController.addComment);
-  app.put(
-    "/api/uncomment/:commentId",
-    authenticate,
-    EventController.deleteComment
-  );
+    //all comment routes...
+    app.put("/api/comment/:eventId", authenticate, EventController.addComment);
+    app.put(
+        "/api/uncomment/:commentId",
+        authenticate,
+        EventController.deleteComment
+    );
 
-  //join or not decision
-  app.put("/api/decision/:eventId", authenticate, EventController.AddDecision);
+    //join or not decision
+    app.put(
+        "/api/decision/:eventId",
+        authenticate,
+        EventController.AddDecision
+    );
 
-  //all like routes...
-  app.put("/api/like", authenticate);
-  app.put("/api/unlike", authenticate);
+    //all like routes...
+    app.put("/api/like", authenticate);
+    app.put("/api/unlike", authenticate);
 };
