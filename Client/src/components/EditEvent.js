3a9930da14440c4a67f33a2b6ea71362193b8ce5
anjan bhattarai <iNepaliZy@anjans-MacBook-Pro.local>
2022-06-26 19:24:33 -0700

    fixed some bugs on Launch event, created Edit event page, need to work on fixing the zipcode state and city

--- Client/src/components/EditEvent.js
+++ Client/src/components/EditEvent.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 //Imports from the MUI
 import Box from "@mui/material/Box";
@@ -12,27 +12,54 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 
-function LaunchEvent() {
+function EditEvent() {
     const [eventTitle, setEventTitle] = useState("");
-    const [location, setLocation] = useState({
-        "street": '', "city": '', "state": '', "zipcode": ''
-    });
+    const [location, setLocation] = useState("");
+    const [city, setCity] = useState("");
+    const [state , setState] = useState("");
+    const [zipcode , setZipcode] = useState("");
     const [date, setDate] = useState("0");
     const [time, setTime] = useState("0");
-    // const [image, setImage] = useState("");
+    const [image, setImage] = useState("");
     const [description, setDescription] = useState([]);
 
     //navigate
     const navigate = useNavigate();
+    const {id} = useParams();
+
+    useEffect(()=>{
+        console.log("There it is")
+        axios.get(`http://localhost:8000/api/events/${id}`,{withCredentials:true})
+        .then(res =>{
+            setEventTitle(res.data.event.eventTitle)
+            setLocation(res.data.event.location.street)
+            setCity(res.data.event.location.city)
+            setState(res.data.event.location.state)
+            setZipcode(res.data.event.location.zipcode)
+            setDate(res.data.event.date)
+            setTime(res.data.event.time)
+            setDescription(res.data.event.description)
+            console.log("DATATAT",res.data.event.date)
+        }).catch(err =>{
+            console.log("Error on Edit",err)
+        })
+    }, [])
+
+
+    const eventDate = (date) => {
+        let fixDate = new Date(date);
+        return fixDate.toLocaleDateString();
+    };
+    console.log(eventDate);
 
     //submithandler
     const submitHandler = (e) => {
         e.preventDefault();
+        console.log("hello");
         axios
             .post(
-                "http://localhost:8000/api/events",
-                { eventTitle, location, date, time, description},{ withCredentials: true }
-            )
+                `http://localhost:8000/api/events/${id}`,
+                { eventTitle, location, date, time, description})
             .then((res) => {
                 console.log(res.data);
                 navigate("/");
@@ -42,18 +69,10 @@ function LaunchEvent() {
             });
     };
 
-    const locationHandler = ((e)=>{
-        setLocation({
-            ...location,
-            [e.target.name]: e.target.value
-        })
-    })
-
     return (
-        <div className="eventLaunchBar" >
-
+        <div className="eventLaunchBar">
             <Card sx={{ maxWidth: 450 }} className="boxDetails">
-                <form onSubmit={submitHandler}>
+                <form>
                     <Box
                         sx={{
                             width: 500,
@@ -61,7 +80,7 @@ function LaunchEvent() {
                         }}
                     >
                         <br />
-                        <h1>Launch an Event</h1>
+                        <h1>Edit Event</h1>
                         <TextField
                             fullWidth
                             label="Event Title"
@@ -82,27 +101,23 @@ function LaunchEvent() {
                             fullWidth
                             label="Street"
                             id="fullWidth"
-                            name="street"
-                            value={location.street}
-                            onChange={locationHandler}
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
                         />
                     </Box>
                     <br />
                     <Grid container item spacing={3}>
                         <Grid item xs={6}>
-                            <TextField fullWidth label="City" id="fullWidth" name="city" value={location.city} onChange={locationHandler}/>
+                            <TextField fullWidth label="City" id="fullWidth" />
                         </Grid>
                         <Grid item xs={3}>
-                            <TextField fullWidth label="State" id="fullWidth"  name="state" value={location.state} onChange={locationHandler}/>
+                            <TextField fullWidth label="State" id="fullWidth" />
                         </Grid>
                         <Grid item xs={3}>
                             <TextField
                                 fullWidth
                                 label="ZipCode"
                                 id="fullWidth"
-                                name="zipcode"
-                                value={location.zipcode}
-                                onChange={locationHandler}
                             />
                         </Grid>
                     </Grid>
@@ -124,7 +139,7 @@ function LaunchEvent() {
                     <br />
                     <input
                         type="date"
-                        value={date}
+                        value={eventDate(date)}
                         onChange={(e) => setDate(e.target.value)}
                     />
                     <input
@@ -139,8 +154,8 @@ function LaunchEvent() {
                         direction="row"
                         className="eventLaunchButton"
                     >
-                        <Button variant="outlined" type= "submit">
-                            Create an Event
+                        <Button variant="outlined" onClick={submitHandler}>
+                            Update Event
                         </Button>
                     </Stack>
                     <br />
@@ -150,4 +165,4 @@ function LaunchEvent() {
     );
 }
 
-export default LaunchEvent;
+export default EditEvent;
