1aeed5c65e4c595e3d4c82682b34a21917fd28b7
katoltmans <katoltmans@msn.com>
2022-06-27 14:43:17 -0400

    Updated comment CSS

--- Client/src/components/EventComments.js
+++ Client/src/components/EventComments.js
@@ -1,95 +1,126 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
-  Button,
-  IconButton,
-  Paper,
-  TextField,
-  Typography,
+    Button,
+    Grid,
+    IconButton,
+    Paper,
+    TextField,
+    Typography,
 } from "@mui/material";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 const EventComments = (props) => {
-  const { comments, setComments, id, user } = props;
-  const [comment, setComment] = useState("");
+    const { comments, setComments, id, user } = props;
+    const [comment, setComment] = useState("");
 
-  const onSubmitComment = () => {
-    axios
-      .put(
-        `http://localhost:8000/api/comment/${id}`,
-        {
-          details: comment,
-        },
-        {
-          withCredentials: true,
-        }
-      )
-      .then((res) => {
-        console.log(res);
-        setComments(res.data.comments);
-        setComment("");
-      })
-      .catch((err) => {
-        console.log("Error with EventController comments request", err);
-      });
-  };
-  console.log("comments", comments);
-  const deleteComment = (commentId) => {
-    axios
-      .put(
-        `http://localhost:8000/api/uncomment/${commentId}`,
-        {
-          eventId: id,
-        },
-        {
-          withCredentials: true,
-        }
-      )
-      .then((res) => {
-        console.log(res);
-        setComments(comments.filter((c) => c._id !== commentId));
-      })
-      .catch((err) => {
-        console.log("Error with delete request", err);
-      });
-  };
+    const onSubmitComment = () => {
+        axios
+            .put(
+                `http://localhost:8000/api/comment/${id}`,
+                {
+                    details: comment,
+                },
+                {
+                    withCredentials: true,
+                }
+            )
+            .then((res) => {
+                console.log(res);
+                setComments(res.data.comments);
+                setComment("");
+            })
+            .catch((err) => {
+                console.log("Error with EventController comments request", err);
+            });
+    };
+    console.log("comments", comments);
+    const deleteComment = (commentId) => {
+        axios
+            .put(
+                `http://localhost:8000/api/uncomment/${commentId}`,
+                {
+                    eventId: id,
+                },
+                {
+                    withCredentials: true,
+                }
+            )
+            .then((res) => {
+                console.log(res);
+                setComments(comments.filter((c) => c._id !== commentId));
+            })
+            .catch((err) => {
+                console.log("Error with delete request", err);
+            });
+    };
 
-  return (
-    <Paper elevation={2} sx={{ p: 3 }}>
-      <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
-        Comments
-      </Typography>
-      <ul>
-        {comments.map((c) => {
-          return (
-            <li key={c._id}>
-              {c.postedBy.firstName + " " + c.postedBy.lastName}
-
-              {c.details}
-              {c.postedBy._id === user._id ? (
-                <IconButton onClick={() => deleteComment(c._id)}>
-                  <HighlightOffIcon sx={{ color: "#992e2e" }} />
-                </IconButton>
-              ) : null}
-            </li>
-          );
-        })}
-      </ul>
-      <form>
-        <TextField
-          fullWidth
-          label="Comments"
-          id="div"
-          className="textField"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <Button variant="contained" sx={{ mt: 2 }} onClick={onSubmitComment}>
-          Submit
-        </Button>
-      </form>
-    </Paper>
-  );
+    return (
+        <Paper elevation={2} sx={{ p: 3 }}>
+            <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
+                Comments
+            </Typography>
+            <form>
+                <TextField
+                    fullWidth
+                    label="Comments"
+                    id="div"
+                    className="textField"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                />
+                <Button
+                    variant="contained"
+                    sx={{ mt: 1 }}
+                    onClick={onSubmitComment}
+                >
+                    Submit
+                </Button>
+            </form>
+            <ul>
+                {comments.map((c) => {
+                    return (
+                        <li key={c._id}>
+                            <Grid container>
+                                <Grid item xs={11}>
+                                    <Typography
+                                        variant="h5"
+                                        component="h2"
+                                        sx={{
+                                            fontSize: 15,
+                                            fontWeight: "bold",
+                                            mt: 1,
+                                        }}
+                                    >
+                                        {c.postedBy.firstName +
+                                            " " +
+                                            c.postedBy.lastName}
+                                    </Typography>
+                                </Grid>
+                                {c.postedBy._id === user._id ? (
+                                    <Grid item xs={1}>
+                                        <IconButton
+                                            onClick={() => deleteComment(c._id)}
+                                        >
+                                            <HighlightOffIcon
+                                                sx={{
+                                                    color: "#992e2e",
+                                                    fontSize: 20,
+                                                    fontWeight: "bold",
+                                                    m: -8,
+                                                }}
+                                            />
+                                        </IconButton>
+                                    </Grid>
+                                ) : null}
+                            </Grid>
+                            {c.details}
+                        </li>
+                    );
+                })}
+            </ul>
+        </Paper>
+    );
 };
 
 export default EventComments;
