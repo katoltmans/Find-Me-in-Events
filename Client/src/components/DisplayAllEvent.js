dfd4f5dfb55257693ab83a3b45a6feba3c14df95
Jamal Muniru <jamalmoham10@gmail.com>
2022-06-29 09:51:26 -0400

    made changes to event cards

--- Client/src/components/DisplayAllEvent.js
+++ Client/src/components/DisplayAllEvent.js
@@ -127,38 +127,40 @@ for(let i=0; i<reset.length; i++){
       />
       {eventList.map((event, i) => (
         <div key={event._id} className="displayAll"  >
-          <div id="ev" className=""  onMouseOver={(e) => {handlehover(e, i)}} onMouseOut = {(e) => {out(e)}} >
-          <Card sx={{ maxWidth: 450 }}  >
-            
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image={event.image}
-                alt={event.image}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {event.eventTitle}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {`${event.location.street}, ${event.location.city}, ${event.location.state}, ${event.location.zipcode}`}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {eventDate(event.date)} - <span className="spn">Created by: {event.createdBy.firstName} {event.createdBy.lastName}</span>
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {event.time}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                <Link to={`/events/${event._id}`}> View Details</Link>
-              </Button>
-            </CardActions>
-          </Card>
-          </div>
+          <Link className="link" to={`/events/${event._id}`}>
+            <div id="ev" className=""  onMouseOver={(e) => {handlehover(e, i)}} onMouseOut = {(e) => {out(e)}} >
+              <Card sx={{ maxWidth: 450 }}  >
+                
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={event.image}
+                    alt={event.image}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {event.eventTitle}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {`${event.location.street}, ${event.location.city}, ${event.location.state}, ${event.location.zipcode}`}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {eventDate(event.date)} - <span className="spn">Created by: {event.createdBy.firstName} {event.createdBy.lastName}</span>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {event.time}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    <Link to={`/events/${event._id}`}> View Details</Link>
+                  </Button>
+                </CardActions>
+              </Card>
+            </div>
+          </Link>
         </div>
       ))}
     </div>
