7dd643d768f546e8ffe66e6ef5f0fb7530d962a9
Veda H Mata <hmata.veda@gmail.com>
2022-06-29 15:50:24 -0400

    reset password

--- Server/Routes/user.routes.js
+++ Server/Routes/user.routes.js
@@ -4,6 +4,8 @@ const userRoutes = (app) => {
   app.post("/api/register", userController.register);
   app.post("/api/login", userController.login);
   app.post("/api/logout", userController.logout);
+  app.put("/api/forgotpassword", userController.forgotPassword);
+  app.put("/api/resetpassword", userController.resetPassword);
 };
 
 module.exports = userRoutes;
