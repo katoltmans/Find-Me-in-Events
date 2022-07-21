d09ceed1c1b408706033bcc8ad66a875cad1059f
katoltmans <katoltmans@msn.com>
2022-07-21 13:00:54 -0400

    force install

--- redeploy.sh
+++ redeploy.sh
@@ -3,12 +3,13 @@ git pull
 pm2 status
 pm2 stop server
 cd Client
-npm install
+npm install --force
 npm run build
 sudo rm -rf /var/www/html 
 sudo mv build /var/www/html
 sudo service nginx restart
 sudo grep -rl localhost /var/www/html | xargs sed -i 's/http:\/\/localhost:8000//g'
 cd ../Server
+npm install --force
 pm2 start server.js
 pm2 status
